34735fc2e15f1420fabbc8c9d9ce3fa22accc5f3
Sean <seanspringmann@comcast.net>
2021-05-15 16:58:27 -0400

    message

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,15 @@
       <v-btn color="teal accent-1" outlined class="text-none"> Resume </v-btn>
     </v-app-bar>
 
-    <v-main class="body"> </v-main>
+    <v-main class="body">
+      <v-row>
+        <v-spacer />
+        <v-col cols="6">
+          <welcome />
+        </v-col>
+        <v-spacer />
+      </v-row>
+    </v-main>
   </v-app>
 </template>
 
@@ -21,10 +29,11 @@
 </style>
 
 <script>
+import Welcome from "@/components/Welcome.vue";
 export default {
   name: "App",
 
-  components: {},
+  components: { Welcome },
 
   data: () => ({
     //
---
+++ src/components/Welcome.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card flat color="transparent">
+    <v-card-text> Hi, my name is </v-card-text>
+    <v-card-title style="word-break: break-word" class="pt-0 text-h4 pb-1">
+      Sean Springmann
+    </v-card-title>
+    <v-card-title
+      style="word-break: break-word"
+      class="pt-0 text-h4 grey--text"
+    >
+      I build things but I am bad :(
+    </v-card-title>
+    <v-card-text>
+      My name is Sean Springmann. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+      qui officia deserunt mollit anim id est laborum.
+    </v-card-text>
+    <v-card-actions>
+      <v-btn outlined color="teal accent-1" x-large class="text-none">
+        Get in touch
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
