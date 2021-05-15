a7d5ff36b1782976c7251c72c345b4e299460913
Sean <seanspringmann@comcast.net>
2021-05-15 16:01:44 -0400

    new1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <v-app>
     <v-app-bar app dark hide-on-scroll elevation="0" color="#0A182F">
-      <v-toolbar-items>
-        <v-btn class="text-none"> About </v-btn>
-        <v-btn class="text-none"> Experience </v-btn>
-        <v-btn class="text-none"> Work </v-btn>
-        <v-btn class="text-none"> Contact </v-btn>
-        <v-btn class="text-none"> Resume </v-btn>
-      </v-toolbar-items>
+      <v-spacer />
+      <v-btn text class="text-none"> About </v-btn>
+      <v-btn text class="text-none"> Experience </v-btn>
+      <v-btn text class="text-none"> Work </v-btn>
+      <v-btn text class="text-none"> Contact </v-btn>
+      <v-btn outlined class="text-none"> Resume </v-btn>
     </v-app-bar>
 
     <v-main class="body">
