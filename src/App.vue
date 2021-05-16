0d658cc39f4f9cf45a036f0ad74727b2e6500705
Sean <seanspringmann@comcast.net>
2021-05-16 17:07:59 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,21 @@
     </v-app-bar>
 
     <v-main class="body">
-      <v-layout justify-end column>
+      <v-layout align-center justify-end column class="social-icons">
         <v-btn icon>
           <v-icon> mdi-facebook </v-icon>
         </v-btn>
         <v-btn icon>
           <v-icon> mdi-facebook </v-icon>
         </v-btn>
+        <div class="bar" />
+      </v-layout>
+
+      <v-layout align-center justify-end column class="email-side">
+        <a href="mailto:seanspringmann@comcast.net" class="email-link"
+          >seanspringmann@comcast.net</a
+        >
+        <div class="bar" />
       </v-layout>
 
       <v-row align="center" class="ma-0" style="height: calc(100vh - 128px)">
@@ -34,6 +42,28 @@
 <style>
 .body {
   background-color: #0a182f;
+}
+
+.social-icons {
+  position: fixed;
+  bottom: 0;
+  left: 30px;
+}
+
+.email-side {
+  position: fixed;
+  bottom: 0;
+  right: 30px;
+}
+
+.email-link {
+  writing-mode: vertical-rl;
+  padding-right: 90px;
+}
+
+.bar {
+  height: 90px;
+  border-left: 1px solid #d0d0d0;
 }
 </style>
 
